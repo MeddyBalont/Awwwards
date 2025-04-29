241db8efce360f5d488bedd1a322ff9ec6ebfbee
MeddyBalont <Meddy.Balont@hotmail.com>
2025-04-29 12:36:24 +0200

    second commint

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
-import { useState, useRef, useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
 import { TiLocationArrow } from "react-icons/ti";
+import { useEffect, useRef, useState } from "react";
 import Button from "./button";
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
      const [currentIndex, setCurrentIndex] = useState(1);
@@ -23,9 +27,67 @@ const Hero = () => {
           setCurrentIndex(upComingVideoIndex);
      };
 
+     useEffect(() => {
+          if (loadedVideos === totalVideos - 1) {
+               setIsLoading(false);
+          }
+     });
+     useGSAP(
+          () => {
+               if (hasClicked) {
+                    gsap.set("#next-video", { visibility: "visible" });
+                    gsap.to("#next-video", {
+                         transformOrigin: "center center",
+                         scale: 1,
+                         width: "100%",
+                         height: "100%",
+                         duration: 1,
+                         ease: "power1.inOut",
+                         onStart: () => {
+                              nextVideoRef.current.play();
+                         },
+                    });
+                    gsap.from("#current-video", {
+                         transformOrigin: "center center",
+                         scale: 0,
+                         duration: 1.5,
+                         ease: "power1.inOut",
+                    });
+               }
+          },
+          { dependencies: [currentIndex], revertOnUpdate: true }
+     );
+
+     useGSAP(() => {
+          gsap.set("#video-frame", {
+               clipPath: `polygon(14% 0, 72% 0, 88% 90%, 0 95%)`,
+               borderRadius: "0 0 40% 10%",
+          });
+          gsap.from("#video-frame", {
+               clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+               borderRadius: "0% 0% 0% 0%",
+               ease: "power1.inOut",
+               scrollTrigger: {
+                    trigger: "#video-frame",
+                    start: "center center",
+                    end: "bottom center",
+                    scrub: true,
+               },
+          });
+     });
+
      const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
      return (
           <div className="relative h-dvh w-screen overflow-x-hidden">
+               {isLoading && (
+                    <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+                         <div className="three-body">
+                              <div className="three-body__dot"></div>
+                              <div className="three-body__dot"></div>
+                              <div className="three-body__dot"></div>
+                         </div>
+                    </div>
+               )}
                <div
                     id="video-frame"
                     className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -58,7 +120,7 @@ const Hero = () => {
                          />
                          <video
                               src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-                              // autoPlay
+                              autoPlay
                               loop
                               muted
                               className="absolute left-0 top-0 size-full object-cover object-center"
@@ -73,7 +135,7 @@ const Hero = () => {
                               <h1 className="special-font hero-heading text-blue-100">
                                    redifi<b>n</b>e
                               </h1>
-                              <p className="mb-5 mx-w-64 font-robert-regular">
+                              <p className="mb-5 mx-w-64 font-robert-regular text-blue-100">
                                    Enter the Metagme Layer
                                    <br />
                                    Unleash the Play Economy
@@ -82,11 +144,17 @@ const Hero = () => {
                                    id="watch-trailer"
                                    title="Watch Trailer"
                                    leftIcon={<TiLocationArrow />}
-                                   containerClass="bg-yellow-300 flex-center gap-1"
+                                   containerClass="!bg-yellow-300 flex-center gap-1"
                               />
                          </div>
                     </div>
                </div>
+               <h1
+                    className="special-font hero-heading absolute bottom-5 right-5 
+                text-black"
+               >
+                    G<b>a</b>ming
+               </h1>
           </div>
      );
 };
